53715dbeb2cfc75aeb016086a9a72d5ceb2bafb5
Jean Carlos Ortiz <jeancarlosortizperez02@gmail.com>
2025-01-16 00:06:19 -0400

    Tipos Anonimos and Tuplas

---
+++ TiposAnonimos/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        // Objeto anonimo
+        var objetoAnonimo = new
+        {
+            nombre = "Jean"
+        };
+
+
+        var people = new[]
+        {
+            new { name = "Jean", Last = "Pereyra" },
+            new { name = "Dolly", Last = "Pereyra"}
+        };
+    }
+}
---
+++ Tuplas/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        // Tupla
+        (int id, string name) products = (5, "Memoria RAM");
+
+        Console.WriteLine($"{products.name} : {products.id}");
+
+        products.name = "Estabilizador";
+
+
+        // Otra forma 
+        var person = (4, "Jean Carlos");
+        Console.WriteLine(person.Item1);
+
+        // Arreglo de tuplas 
+        var people = new[]
+        {
+            (1, "Juan"),
+            (2, "Pedro")
+        };
+
+        foreach(var p in people)
+        {
+            Console.WriteLine(p.Item2);
+        }
+
+        (int id, string name)[] persons = new[]
+        {
+            (1, "Juan"),
+            (2, "Pedro")
+        };
+        foreach (var p in persons)
+        {
+            Console.WriteLine(p.name);
+        }
+    }
+}
