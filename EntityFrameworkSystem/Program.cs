18ddff9262bf5424376841260556da3abd7113f4
Jean Carlos Ortiz <jeancarlosortizperez02@gmail.com>
2025-01-19 21:37:17 -0400

    Entity Framework incomplete

---
+++ EntityFrameworkSystem/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using BD;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+
+        DbContextOptionsBuilder<CsharpDbContext> optionsBuilder = 
+            new DbContextOptionsBuilder<CsharpDbContext>();
+        optionsBuilder.UseMySql("Server=127.0.0.1; Database=CsharpDB; Uid=user1; Pwd=abcd123456;",
+            ServerVersion.AutoDetect("Server=127.0.0.1; Database=CsharpDB; Uid=user1; Pwd=abcd123456;"));
+
+        using (CsharpDbContext context = new CsharpDbContext(optionsBuilder.Options))
+        {
+
+            var beers = context.Beers.ToList();
+
+            foreach (var beer in beers)
+            {
+                Console.WriteLine(beer.Name);
+
+            }
+        }
+         
+    }
+}
